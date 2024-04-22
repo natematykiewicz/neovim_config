50a44464e7a83b60a3ae638f0e06b546296f4346
Nate Matykiewicz <natematykiewicz@gmail.com>
2024-04-22 15:49:47 -0500

    Add default catppuccin integrations
    https://github.com/AstroNvim/astrocommunity/blob/62ab36ab8605c76616e11450e21073b924d0c868/lua/astrocommunity/colorscheme/catppuccin/init.lua

--- lua/community.lua
+++ lua/community.lua
@@ -28,7 +28,31 @@ return {
   { import = "astrocommunity.colorscheme.catppuccin" },
   {
     "catppuccin",
+    ---@type CatppuccinOptions
     opts = {
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dap = true,
+        dap_ui = true,
+        gitsigns = true,
+        illuminate = true,
+        indent_blankline = true,
+        markdown = true,
+        mason = true,
+        native_lsp = true,
+        neotree = true,
+        notify = true,
+        semantic_tokens = true,
+        symbols_outline = true,
+        telescope = true,
+        treesitter = true,
+        ts_rainbow = false,
+        ufo = true,
+        which_key = true,
+        window_picker = true,
+      },
       dim_inactive = {
         enabled = true,
       },
