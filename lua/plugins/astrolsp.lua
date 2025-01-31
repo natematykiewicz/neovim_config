bd572900117c0952a0cc1d0af91f44b1462f98c4
Nate Matykiewicz <natematykiewicz@gmail.com>
2025-01-31 11:11:35 -0600

    Add eruby.html to filetypes

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -113,7 +113,7 @@ return {
         symbols = true,
       },
       html = {
-        filetypes = { "html", "eruby" },
+        filetypes = { "html", "eruby", "eruby.html" },
         init_options = {
           provideFormatter = false,
           embeddedLanguages = { css = true, javascript = true, ruby = true },
