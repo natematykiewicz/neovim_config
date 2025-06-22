feeeca0a4fa8c188c2ead0468ec6660dc33d05e7
Nate Matykiewicz <natematykiewicz@gmail.com>
2025-06-21 23:03:06 -0500

    Disable swapfile, to avoid issue when opening a file twice

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -90,6 +90,7 @@ return {
         spell = true, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = true, -- sets vim.opt.wrap
+        swapfile = false,
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
