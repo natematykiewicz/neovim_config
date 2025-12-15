e41e268d0666d183fe5d97e79bd78a7e2b8f8d4f
Nate Matykiewicz <natematykiewicz@gmail.com>
2025-12-15 16:01:54 -0600

    Delete trailing whitespace on save

--- lua/polish.lua
+++ lua/polish.lua
@@ -40,3 +40,13 @@ vim.api.nvim_create_autocmd("FileType", {
     if vim.bo.filetype == "eruby.yaml" then vim.bo.filetype = "yaml" end
   end,
 })
+
+-- Delete trailing whitespace on save
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*" },
+  callback = function()
+    local save_cursor = vim.fn.getpos "."
+    pcall(function() vim.cmd [[%s/\s\+$//e]] end)
+    vim.fn.setpos(".", save_cursor)
+  end,
+})
